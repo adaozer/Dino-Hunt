bf7ec47685ee9ffda37bc0395710e92be6d78d54
adaozer <adaozer1@gmail.com>
2025-11-24 13:16:23 +0000

    Add project files.

---
+++ window/game.cpp
@@ -0,0 +1,17 @@
+#include "window.h"
+#include "core.h"
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) {
+	Window win;
+	Core core;
+	win.init(1024, 1024, 0, 0, "My Window");
+	core.init(win.hwnd, 1024, 1024);
+	while (true) {
+		core.beginFrame();
+		win.processMessages();
+		if (win.keys[VK_ESCAPE] == 1) break;
+		core.finishFrame();
+	}
+	core.flushGraphicsQueue();
+	return 0;
+}
---
+++ window/window.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#define NOMINMAX
+#include <Windows.h>
+#include <string>
+
+#define WINDOW_GET_X_LPARAM(lp) ((int)(short)LOWORD(lp))
+#define WINDOW_GET_Y_LPARAM(lp) ((int)(short)HIWORD(lp))
+
+class Window {
+public:
+	HWND hwnd;
+	HINSTANCE hinstance;
+	float width, height;
+	std::string name;
+
+	bool keys[256];
+	int mousex;
+	int mousey;
+	bool mouseButtons[3];
+
+	void init(float window_height, float window_width, float window_x, float window_y, std::string window_name);
+
+	void updateMouse(int x, int y);
+
+	void processMessages();
+};
