1a25af3bf17b2861fc8e5f148dc90b2d62b0172a
adaozer <adaozer1@gmail.com>
2025-12-09 16:10:08 +0000

    camera

--- window/game.cpp
+++ window/game.cpp
@@ -20,10 +20,9 @@ void listAnimationNames(const GEMLoader::GEMAnimation& gemanimation)
 {
 	for (int i = 0; i < gemanimation.animations.size(); i++)
 	{
-		printf((gemanimation.animations[i].name).c_str(), "\n");
+		std::cout << gemanimation.animations[i].name << std::endl;
 	}
 }
-
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) {
 	Window win;
@@ -34,12 +33,12 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nC
 	ShaderManager shaderManager;
 	TextureManager tex;
 
-	AllocConsole();
+	/*AllocConsole();
 	FILE* stream;
 	freopen_s(&stream, "CONOUT$", "w", stdout);
-
-	AnimatedModel am(&shaderManager, &tex, "textures/T-rex_Base_Color_alb.png");
-	am.load(&core, "models/TRex.gem");
+	*/
+	AnimatedModel am(&shaderManager, &tex, "textures/T_Animalstextures_alb.png");
+	am.load(&core, "models/Cat-Siamese.gem");
 
 	AnimationInstance animatedInstance;
 	animatedInstance.init(&am.animation, 0);
@@ -82,46 +81,60 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nC
 
 		if (win.mouseButtons[0]) {
 			cam.yaw += win.dx * cam.sensitivity;
-			cam.pitch += win.dy * cam.sensitivity;
+			/*cam.pitch += win.dy * cam.sensitivity;
 
 			float boundary = (89.f * M_PI) / 180.f;
 			if (cam.pitch > boundary) cam.pitch = boundary;
 			if (cam.pitch < -boundary) cam.pitch = -boundary;
-
+			*/
 			win.dx = 0;
 			win.dy = 0;
 		}
 
-		Vec3 front = cam.front();
-		Vec3 right = cam.right();
+		float yaw = cam.yaw;
 
-		if (win.keys['W']) cam.pos += front * cam.moveSpeed * dt;
-		if (win.keys['A']) cam.pos -= right * cam.moveSpeed * dt;
-		if (win.keys['S']) cam.pos -= front * cam.moveSpeed * dt;
-		if (win.keys['D']) cam.pos += right * cam.moveSpeed * dt;
+		Vec3 forward = Vec3(std::cos(yaw), 0.0f, std::sin(yaw));
+		Vec3 right = Vec3(-std::sin(yaw), 0.0f, std::cos(yaw));
+
+		Vec3 move(0.f, 0.f, 0.f);
+
+		if (win.keys['W']) move += forward;
+		if (win.keys['A']) move += right;
+		if (win.keys['S']) move -= forward;
+		if (win.keys['D']) move -= right;
+
+		if (move.lengthSquare() > 0.f) {
+			move = move.normalize();
+			cam.pos += move * cam.moveSpeed * dt;
+		}
+		cam.pos.y = 2.5;
 
 		Matrix v = cam.viewMatrix();
+		Matrix camWorld = v.invert();
+		Vec3 offsetCam = Vec3(0, -2.f, 4.5);
 		Matrix vp = v * p;
-		Matrix W;
+		Matrix local = Matrix::scale(Vec3(0.01f, 0.01f, 0.01f)) * Matrix::rotateY(M_PI) * Matrix::translate(offsetCam) ;
 
 		core.beginRenderPass();
 
-		animatedInstance.update("run", dt);
+		animatedInstance.update("run forward", dt);
 		animatedInstance1.update("firing rifle", dt);
 		animatedInstance2.update("08 fire", dt);
 
 		if (animatedInstance1.animationFinished() == true) animatedInstance1.resetAnimationTime();
 		if (animatedInstance.animationFinished() == true) animatedInstance.resetAnimationTime();
 		if (animatedInstance2.animationFinished() == true) animatedInstance2.resetAnimationTime();
-		
-		W = Matrix::scale(Vec3(0.01f, 0.01f, 0.01f));
+
+		Matrix W = local * camWorld;
+		//W = Matrix::scale(Vec3(0.01f, 0.01f, 0.01f));
 		am.draw(&core, &animatedInstance, W, vp);
 
 		W = Matrix::scale(Vec3(0.01f, 0.01f, 0.01f)) * Matrix::translate(Vec3(5, 0, 0));
 		am1.draw(&core, &animatedInstance1, W, vp);
 
-		W = Matrix::scale(Vec3(0.02f, 0.02f, 0.02f)) * Matrix::translate(Vec3(5.2, 0.55, -0.15));
-		//W = Matrix::scale(Vec3(0.01f, 0.01f, 0.01f)) * Matrix::translate(Vec3(15, 0, 0));
+		//W = Matrix::scale(Vec3(0.02f, 0.02f, 0.02f)) * Matrix::translate(Vec3(5.2, 0.55, -0.15));
+		
+		W = Matrix::scale(Vec3(0.01f, 0.01f, 0.01f)) * Matrix::translate(Vec3(15, 0, 0));
 		am2.draw(&core, &animatedInstance2, W, vp);
 
 		W = Matrix::scale(Vec3(0.01f, 0.01f, 0.01f)) * Matrix::translate(Vec3(10, 0, 0));
